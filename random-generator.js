ac2f5ca16e57b070d3379bc9ee6b4e938c780bff
Dmitri Kunin <info@dkunin.ru>
2018-12-30 22:23:19 +0300

    Update

--- random-generator.js
+++ random-generator.js
@@ -1,39 +1,34 @@
 'use strict';
 
-const fs = require('fs');
 const monsters = require('./data/monsters-n.js');
 const meow = require('meow');
 const uniqueRandomArray = require('unique-random-array');
 
 // console.log(random(), random(), random(), random());
 
-const cli = meow(
-    ``,
-    {
-        alias: {
-            c: 'challange'
-        }
+const cli = meow('', {
+    alias: {
+        c: 'challange'
     }
+});
+
+const classes = Object.keys(
+    monsters.reduce((newArray, singleItem) => {
+        newArray[singleItem.type] = 1;
+        return newArray;
+    }, {})
+).filter(Boolean);
+
+const monstersChallenge = uniqueRandomArray(
+    monsters.filter(singleMonster => {
+        return singleMonster['challenge_rating'] < cli.flags.c;
+    })
 );
 
-const classes = Object.keys(monsters.reduce((newArray, singleItem) => {
-	newArray[singleItem.type] = 1;
-	return newArray;
-}, {})).filter(Boolean);
-
-const monstersChallenge = uniqueRandomArray(monsters.filter(singleMonster => {
-	return singleMonster['challenge_rating'] < cli.flags.c;
-}));s
-
-console.log(monstersChallenge());
-console.log(monstersChallenge());
-console.log(monstersChallenge());
-console.log(monstersChallenge());
+// console.log(monstersChallenge());
 
 // if (!cli.input.length) {
 //     cli.showHelp(-1);
 // }
 
-
 // console.log(monsters);
-
